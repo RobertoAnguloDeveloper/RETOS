9c9aace7ef1dc406ae262b4c249f5ef606b19c97
RobertoAnguloDeveloper <rangulotl@outlook.com>
2022-05-01 20:41:18 -0500

    PHP, Python y C#(No olvidar comandos dotnet new console y dotnet run)

---
+++ MayorMenor/MayorMenor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MayorMenor
+{
+    class MayorMenor
+    {
+        public static void Main(string[] args){
+            int num1, num2;
+            Console.WriteLine("Ingrese el primer numero: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero: ");
+            num2 = int.Parse(Console.ReadLine());
+            if(num1 > num2){
+                Console.WriteLine("El numero mayor es: " + num1 + " y la diferencia entre ambos es: "+ (num1-num2));
+            }else if(num1 == num2){
+                Console.WriteLine("Los numeros son iguales y la diferencia entre ambos es: 0");
+            }else{
+                Console.WriteLine("El numero mayor es: " + num2 + " y la diferencia entre ambos es: "+ (num2-num1));
+            }
+
+        }
+    }
+}
