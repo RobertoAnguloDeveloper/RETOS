a183e7beb7e60981d84a5e9d4294dafba96a4f9e
RobertoAnguloDeveloper <rangulotl@outlook.com>
2022-05-01 20:58:31 -0500

    PHP, Python, C#(No olvidar comandos dotnet new console y dotnet run) y JAVA

---
+++ MayorMenor.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MayorMenor{
+    public static void main(String[] args){
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer numero");
+        int num1 = keyboard.nextInt();
+        
+        System.out.println("Ingrese el segundo numero");
+        int num2 = keyboard.nextInt();
+
+        if(num1 > num2){
+            System.out.println("El numero mayor es: " + num1 + " y la diferencia entre ambos es: " + (num1 - num2));
+        }else if(num1 == num2){
+            System.out.println("Los numeros son iguales y la diferencia entre ambos es: 0");
+        }else{
+            System.out.println("El numero mayor es: " + num2 + " y la diferencia entre ambos es: " + (num2 - num1));
+        }
+        
+    }
+}
