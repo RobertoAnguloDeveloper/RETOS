d7a12de158089106093086f84246a70d281fa834
RobertoAnguloDeveloper <rangulotl@outlook.com>
2022-05-01 19:20:04 -0500

    PHP, Python y C#2

---
+++ MayorMenor/mayormenor.cs
@@ -0,0 +1,14 @@
+//Hola mundo
+
+using System;
+
+namespace MayorMenor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+        }
+    }
+}
